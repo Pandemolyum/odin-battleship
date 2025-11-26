97b8da47a02d595d030e2dbe929a5829abc115bb
Pandemolyum <77362203+Pandemolyum@users.noreply.github.com>
2025-11-26 11:49:09 -0500

    Add grid cell identification on click

--- src/dom-controller.js
+++ src/dom-controller.js
@@ -1,9 +1,18 @@
+// Creates event listeners
 function createEventListeners() {
     document.addEventListener("click", (e) => {
-        if (e.target) {
-            console.log(e.target);
+        if (e.target.classList.contains("cell")) {
+            const parent = e.target.parentNode;
+            const children = parent.children;
+            const i = Array.prototype.indexOf.call(children, e.target);
+            console.log(parent.parentNode.classList[0]);
+            console.log(gridIndexToCoords(i));
         }
     });
 }
 
-export { createEventListeners };
+function gridIndexToCoords(gridIndex) {
+    return [Math.floor(gridIndex / 10), gridIndex % 10];
+}
+
+export { createEventListeners, gridIndexToCoords };

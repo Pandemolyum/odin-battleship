e1e54d940a8e7c7c4177c31a31046927b19ce186
Pandemolyum <77362203+Pandemolyum@users.noreply.github.com>
2025-11-27 10:22:54 -0500

    Fix unresponsive game when all squares hit by AI

--- src/player.js
+++ src/player.js
@@ -12,14 +12,19 @@ class Player {
 
     // Sends a random attack to the enemy board
     sendAttack(board) {
-        let x, y;
-        do {
-            x = Math.floor(Math.random() * 10);
-            y = Math.floor(Math.random() * 10);
-        } while (this.hitCoords.some((elem) => elem[0] === x && elem[1] === y));
+        // Ensure that there are squares left
+        if (this.hitCoords.length <= BOARD_SIZE ** 2) {
+            let x, y;
+            do {
+                x = Math.floor(Math.random() * 10);
+                y = Math.floor(Math.random() * 10);
+            } while (
+                this.hitCoords.some((elem) => elem[0] === x && elem[1] === y)
+            );
 
-        this.hitCoords.push([x, y]);
-        board.receiveAttack([x, y]);
+            this.hitCoords.push([x, y]);
+            board.receiveAttack([x, y]);
+        }
     }
 }
 
